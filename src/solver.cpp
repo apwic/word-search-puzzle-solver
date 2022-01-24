5c170c38473bb8a03ea12f6a3e817272ac3ac7f6
apwic <16520227@mahasiswa.itb.ac.id>
2022-01-24 23:09:33 +0700

    Format output and add file validation

--- src/solver.cpp
+++ src/solver.cpp
@@ -4,289 +4,369 @@
 #include <string>
 using namespace std;
 
-void printHash(vector<vector<char>> puzzle, vector<vector<bool>> puzzleHash){
+void printHash(vector<vector<char>> puzzle, vector<vector<bool>> puzzleHash)
+{
     int i, j;
     int row = puzzle.size();
     int col = puzzle[0].size();
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
-            if (puzzleHash[i][j] == true){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (puzzleHash[i][j] == true)
+            {
                 cout << puzzle[i][j] << " ";
-            } else {
+            }
+            else
+            {
                 cout << "_ ";
             }
         }
         cout << endl;
     }
+
+    cout << endl;
 }
 
-void searchHorizontalRight(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchHorizontalRight(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
-            for (k = j; k - j < wordSize; k++){
-                if (k >= col) {
+            for (k = j; k - j < wordSize; k++)
+            {
+                if (k >= col)
+                {
                     break;
                 }
 
-                if (puzzle[i][k] == word[k-j]){
+                if (puzzle[i][k] == word[k - j])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
-        for (j = q; j-q < wordSize; j++){
+    if (found)
+    {
+
+        for (j = q; j - q < wordSize; j++)
+        {
             puzzleHash[p][j] = true;
         }
         printHash(puzzle, puzzleHash);
     }
 }
 
-void searchHorizontalLeft(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchHorizontalLeft(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
-            for (k = j; j - k < wordSize; k--){
-                if (k < 0) {
+            for (k = j; j - k < wordSize; k--)
+            {
+                if (k < 0)
+                {
                     break;
                 }
 
-                if (puzzle[i][k] == word[j-k]){
+                if (puzzle[i][k] == word[j - k])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
-        for (j = q; q - j < wordSize; j--){
+    if (found)
+    {
+
+        for (j = q; q - j < wordSize; j--)
+        {
             puzzleHash[p][j] = true;
         }
         printHash(puzzle, puzzleHash);
     }
 }
 
-void searchVerticalUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchVerticalUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
-            for (k = i; i - k < wordSize; k--){
-                if (k < 0) {
+            for (k = i; i - k < wordSize; k--)
+            {
+                if (k < 0)
+                {
                     break;
                 }
 
-                if (puzzle[k][j] == word[i-k]){
+                if (puzzle[k][j] == word[i - k])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
-        for (i = p; p - i < wordSize; i--){
+    if (found)
+    {
+
+        for (i = p; p - i < wordSize; i--)
+        {
             puzzleHash[i][q] = true;
         }
         printHash(puzzle, puzzleHash);
     }
 }
 
-void searchVerticalDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchVerticalDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
-            for (k = i; k - i < wordSize; k++){
-                if (k >= row) {
+            for (k = i; k - i < wordSize; k++)
+            {
+                if (k >= row)
+                {
                     break;
                 }
 
-                if (puzzle[k][j] == word[k-i]){
+                if (puzzle[k][j] == word[k - i])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
-        for (i = p; i - p < wordSize; i++){
+    if (found)
+    {
+
+        for (i = p; i - p < wordSize; i++)
+        {
             puzzleHash[i][q] = true;
         }
         printHash(puzzle, puzzleHash);
     }
 }
 
-void searchDiagonalRightUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchDiagonalRightUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k, l;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
             k = i;
             l = j;
-            while (i - k < wordSize && l - j < wordSize) {
-                if (k < 0 || l >= col) {
+            while (i - k < wordSize && l - j < wordSize)
+            {
+                if (k < 0 || l >= col)
+                {
                     break;
                 }
 
-                if (puzzle[k][l] == word[i-k]){
+                if (puzzle[k][l] == word[i - k])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
                 k--;
                 l++;
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
+    if (found)
+    {
+
         i = p;
         j = q;
-        while (p - i < wordSize && j - q < wordSize){
+        while (p - i < wordSize && j - q < wordSize)
+        {
             puzzleHash[i][j] = true;
             i--;
             j++;
-        }   
+        }
         printHash(puzzle, puzzleHash);
-    }        
+    }
 }
 
-void searchDiagonalRightDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchDiagonalRightDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k, l;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
             k = i;
             l = j;
-            while (k - i < wordSize && l-j < wordSize) {
-                if (k >= row || l >= col) {
+            while (k - i < wordSize && l - j < wordSize)
+            {
+                if (k >= row || l >= col)
+                {
                     break;
                 }
 
-                if (puzzle[k][l] == word[k-i]){
+                if (puzzle[k][l] == word[k - i])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
                 k++;
                 l++;
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
+    if (found)
+    {
+
         i = p;
         j = q;
-        while (i - p < wordSize && j - q < wordSize){
+        while (i - p < wordSize && j - q < wordSize)
+        {
             puzzleHash[i][j] = true;
             i++;
             j++;
@@ -295,100 +375,124 @@ void searchDiagonalRightDown(vector<vector<char>> puzzle, int row, int col, stri
     }
 }
 
-void searchDiagonalLeftUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchDiagonalLeftUp(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k, l;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
             k = i;
             l = j;
-            while (i - k < wordSize && j - l < wordSize) {
-                if (k < 0 || l < 0) {
+            while (i - k < wordSize && j - l < wordSize)
+            {
+                if (k < 0 || l < 0)
+                {
                     break;
                 }
 
-                if (puzzle[k][l] == word[i-k]){
+                if (puzzle[k][l] == word[i - k])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
                 k--;
                 l--;
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
+    if (found)
+    {
+
         i = p;
         j = q;
-        while (p - i < wordSize && q - j < wordSize){
+        while (p - i < wordSize && q - j < wordSize)
+        {
             puzzleHash[i][j] = true;
             i--;
             j--;
-        }   
+        }
         printHash(puzzle, puzzleHash);
-    }   
+    }
 }
 
-void searchDiagonalLeftDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash){
+void searchDiagonalLeftDown(vector<vector<char>> puzzle, int row, int col, string word, vector<vector<bool>> puzzleHash)
+{
     bool found = false;
     int wordSize = word.size();
     int wordCount = 0;
     int i, j, k, l;
     int p, q;
 
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             wordCount = 0;
             k = i;
             l = j;
-            while (k - i < wordSize && j - l < wordSize) {
-                if (k >= row || l < 0) {
+            while (k - i < wordSize && j - l < wordSize)
+            {
+                if (k >= row || l < 0)
+                {
                     break;
                 }
 
-                if (puzzle[k][l] == word[j-l]){
+                if (puzzle[k][l] == word[j - l])
+                {
                     wordCount++;
-                } else {
+                }
+                else
+                {
                     break;
                 }
                 k++;
                 l--;
             }
 
-            if (wordCount == wordSize){
+            if (wordCount == wordSize)
+            {
                 found = true;
                 q = j;
                 break;
             }
         }
-        if (found){
+        if (found)
+        {
             p = i;
             break;
         }
     }
 
-    if (found) {
-        cout << p << " " << q << endl;
+    if (found)
+    {
+
         i = p;
         j = q;
-        while (i - p < wordSize && q - j < wordSize){
+        while (i - p < wordSize && q - j < wordSize)
+        {
             puzzleHash[i][j] = true;
             i++;
             j--;
--- src/word_puzzle_search.cpp
+++ src/word_puzzle_search.cpp
@@ -5,17 +5,21 @@
 #include "solver.cpp"
 using namespace std;
 
-vector<vector<char>> getPuzzle(ifstream& InputText){
+vector<vector<char>> getPuzzle(ifstream &InputText)
+{
     vector<vector<char>> puzzle;
     vector<char> puzzleLine;
     char input;
     int countNewLine = 0;
 
-    while (InputText.get(input)){
-        if (input == '\n'){
+    while (InputText.get(input))
+    {
+        if (input == '\n')
+        {
             countNewLine++;
 
-            if (countNewLine == 2){
+            if (countNewLine == 2)
+            {
                 break;
             }
             puzzle.push_back(puzzleLine);
@@ -26,7 +30,8 @@ vector<vector<char>> getPuzzle(ifstream& InputText){
 
         countNewLine = 0;
 
-        if (input != ' '){
+        if (input != ' ')
+        {
             puzzleLine.push_back(input);
         }
     }
@@ -34,28 +39,48 @@ vector<vector<char>> getPuzzle(ifstream& InputText){
     return puzzle;
 }
 
-vector<string> getWord(ifstream& InputText){
+vector<string> getWord(ifstream &InputText)
+{
     vector<string> word;
     char input;
     string text;
 
-    while (InputText.get(input)){
-        if (input == '\n'){
+    while (InputText.get(input))
+    {
+        if (input == '\n')
+        {
             word.push_back(text);
             text = "";
         }
 
-        else {
+        else
+        {
             text.push_back(input);
         }
-
     }
     return word;
 }
 
-int main(){
+int main()
+{
+    string fileName;
+    cout << "Input your file name: ";
+    cin >> fileName;
+
     // Read from the text file
-    ifstream InputText("./test/test_1.txt");
+    string path;
+    path = "../test/" + fileName;
+    ifstream InputText(path);
+
+    while (!InputText){
+        cout << "File name doesn't exist, please try again!" << endl;
+        cout << "Input your file name: ";
+        cin >> fileName; 
+        path = "../test/" + fileName;
+        InputText.clear();
+        InputText.seekg(0, InputText.beg);
+        InputText.open(path);
+    }
 
     // Get Puzzle
     vector<vector<char>> puzzle = getPuzzle(InputText);
@@ -65,9 +90,11 @@ int main(){
 
     // Create Boolean Hash Table
     vector<vector<bool>> puzzleHash;
-    for (int i = 0; i < puzzle.size(); i++){
+    for (int i = 0; i < puzzle.size(); i++)
+    {
         puzzleHash.push_back({});
-        for (int j = 0; j < puzzle[0].size(); j++){
+        for (int j = 0; j < puzzle[0].size(); j++)
+        {
             puzzleHash[i].push_back(false);
         }
     }
@@ -84,7 +111,9 @@ int main(){
     //     cout << word[i] << endl;
     // }
 
-    for (int i = 0; i < word.size(); i++){
+    for (int i = 0; i < word.size(); i++)
+    {
+        cout << "Word " << i + 1 << ": " << endl << endl;
         searchHorizontalRight(puzzle, puzzle.size(), puzzle[0].size(), word[i], puzzleHash);
         searchHorizontalLeft(puzzle, puzzle.size(), puzzle[0].size(), word[i], puzzleHash);
         searchVerticalUp(puzzle, puzzle.size(), puzzle[0].size(), word[i], puzzleHash);
@@ -94,7 +123,6 @@ int main(){
         searchDiagonalLeftUp(puzzle, puzzle.size(), puzzle[0].size(), word[i], puzzleHash);
         searchDiagonalLeftDown(puzzle, puzzle.size(), puzzle[0].size(), word[i], puzzleHash);
     }
-
 
     InputText.close();
 }
